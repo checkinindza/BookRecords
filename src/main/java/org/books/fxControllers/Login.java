64b5bc1ce32b2120ffc288e92279a8585b1b1eef
checkinindza <tomasfilipavicius13@gmail.com>
2024-10-09 08:57:51 +0300

    GUI Work

--- src/main/java/org/books/fxControllers/Login.java
+++ src/main/java/org/books/fxControllers/Login.java
@@ -2,7 +2,29 @@ package org.books.fxControllers;
 
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.books.StartGUI;
+
+import java.io.IOException;
 
 public class Login {
     EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("coursework-shop");
+    @FXML
+    public TextField loginField;
+    @FXML
+    public PasswordField pswField;
+
+    public void openNewWindow() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(StartGUI.class.getResource("/org.books/main.fxml"));
+        Stage stage = (Stage) loginField.getScene().getWindow();
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Book exchange");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
---
+++ src/main/java/org/books/fxControllers/Register.java
@@ -0,0 +1,4 @@
+package org.books.fxControllers;
+
+public class Register {
+}
